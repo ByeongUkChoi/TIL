a15d4e86dcac1cf84588e78c53195aec0d509b3b
ChoiBU <cbw0916@gmail.com>
2018-10-06 01:00:28 +0900

    fix deploy file

--- deploy_without_build.sh
+++ deploy_without_build.sh
@@ -9,12 +9,15 @@ set -e
 # navigate into the build output directory
 cd docs/.vuepress/dist
 
+# remove git init
+rm -rf .git
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m "$1" 
+git commit -m "$1 -deploy" 
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
